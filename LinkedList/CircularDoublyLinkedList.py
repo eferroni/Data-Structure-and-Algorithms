0164093b1a9727adcedc7b409867f40d1231bcab
Eduardo Ferroni <edu_ferroni@hotmail.com>
2021-10-19 23:31:00 -0300

    circular linked list study and new linked list class for exercises

--- LinkedList/CircularDoublyLinkedList.py
+++ LinkedList/CircularDoublyLinkedList.py
@@ -60,6 +60,95 @@ class CircularDoublyLinkedList:
                 if tempNode == self.tail:
                     self.tail = node
 
+    def traverseCDLL(self):
+        if self.head is None:
+            print("not exist")
+        else:
+            node = self.head
+            while node:
+                print(node.value)
+                if node == self.tail:
+                    break
+                node = node.next
+
+    def reverseTraverseCDLL(self):
+        if self.head is None:
+            print("not exist")
+        else:
+            node = self.tail
+            while node is not None:
+                print(node.value)
+                if node == self.head:
+                    break
+                node = node.prev
+
+    def searchCDLL(self, value):
+        if self.head is None:
+            return "not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == value:
+                    return f"{value}: founded"
+                if node == self.tail:
+                    break
+                node = node.next
+            return f"{value}: not found"
+
+    def deleteCDLL(self, location):
+        if self.head is None:
+            return "empty singly list"
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head.prev = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
+
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head.prev = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                    if tempNode is None:
+                        return False
+                if tempNode.next is None:
+                    return False
+                deleteNode = tempNode.next
+                nextNode = deleteNode.next
+                tempNode.next = nextNode
+                nextNode.prev = tempNode
+                if tempNode.next is None:
+                    self.tail = tempNode
+
+    def deleteEntireCDLL(self):
+        if self.head is None:
+            return "empty singly list"
+        else:
+            self.tail.next = None
+            tempNode = self.head
+            while tempNode:
+                tempNode.prev = None
+                tempNode = tempNode.next
+            self.head = None
+            self.tail = None
+
 
 cdll = CircularDoublyLinkedList()
 cdll.createCDLL(1)
@@ -68,4 +157,13 @@ cdll.insertCDLL(3, -1)
 cdll.insertCDLL(4, -1)
 cdll.insertCDLL(0, 0)
 cdll.insertCDLL(2, 2)
+print([node.value for node in cdll])
+# cdll.traverseCDLL()
+# print()
+# cdll.reverseTraverseCDLL()
+print(cdll.searchCDLL(3))
+print(cdll.searchCDLL(5))
+cdll.deleteCDLL(1)
+print([node.value for node in cdll])
+cdll.deleteEntireCDLL()
 print([node.value for node in cdll])
