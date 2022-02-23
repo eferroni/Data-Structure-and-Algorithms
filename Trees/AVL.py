34b227263bb688523a0809c64d920800135622e4
Eduardo Ferroni <edu_ferroni@hotmail.com>
2022-02-22 22:58:16 -0300

    finish binary search tree, start avl tree

---
+++ Trees/AVL.py
@@ -0,0 +1,63 @@
+import QueueLinkedList as queue
+
+
+class AVLNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+        self.height = 1
+
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            print(root.value.data)
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+
+
+def searchNode(rootNode, nodeValue):
+    if rootNode.data == nodeValue:
+        print("FOUND")
+        return
+    elif nodeValue < rootNode.data:
+        if rootNode.leftChild:
+            searchNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild:
+            searchNode(rootNode.rightChild, nodeValue)
+
+
+newAVL = AVLNode(10)
